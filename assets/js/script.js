b875e1241289cc51ad3299f88b9dcbba26d629c1
zion-ziyang <996478975@gmail.com>
2025-11-27 14:00:05 +0800

    Add Life Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,17 +143,26 @@ const pages = document.querySelectorAll("[data-page]");
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
+    const clickedPage = this.innerHTML.toLowerCase();
 
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
+    // Update pages
+    for (let j = 0; j < pages.length; j++) {
+      if (clickedPage === pages[j].dataset.page) {
+        pages[j].classList.add("active");
       } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
+        pages[j].classList.remove("active");
       }
     }
 
+    // Update nav links
+    for (let k = 0; k < navigationLinks.length; k++) {
+      if (clickedPage === navigationLinks[k].innerHTML.toLowerCase()) {
+        navigationLinks[k].classList.add("active");
+      } else {
+        navigationLinks[k].classList.remove("active");
+      }
+    }
+
+    window.scrollTo(0, 0);
   });
 }
